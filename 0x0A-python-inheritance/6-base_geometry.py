11d2bc22d1be8fe07f74ad5b841801a2ff5e514e
Simanga <simacoder@hotmail.com>
2022-10-25 02:57:03 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Contains a function area()
+    """
+
+    def area(self):
+        """Function not implemented yet
+        """
+        raise Exception("area() is not implemented")
