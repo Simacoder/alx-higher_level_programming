0b0297ca68ba523f7ca1db899a19b91dfc37febc
Simanga <simacoder@hotmail.cm>
2023-01-17 13:04:27 -0800

    task 6 effiecit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,7 +8,6 @@ def find_peak(list_of_integers):
     """
     Return a peak in a list of unsorted integers
     """
-
     if len(list_of_integers) == 0:
         return None
     if len(list_of_integers) == 1:
